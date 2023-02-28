96be2fc9e70f3246760de7b56d8a0fc42fda5720
Gijs Koot <gijs@spotr.ai>
2023-02-28 17:02:07 +0100

    add weather data

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ data/raw: ./get-data.sh
 	touch data/raw
 data/agg.json: data/20*.json
 	cat data/20*.json | jq '.data.usages[].entries[].actual | {date: .date, gas_high: .gas.high, gas_low: .gas.low, elec_high: .electricity.high, elec_low: .electricity.low}' | jq -s '.' > data/agg.json
+data/uurgeg_260_2021-2030.zip:
+	wget https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/uurgegevens/uurgeg_260_2021-2030.zip
+data/weather.csv
--- energy.R
+++ energy.R
@@ -1,33 +1,58 @@
-library(readxl)
 library(dplyr)
-
-fn <- "data/verbruik_01-06-2022_01-12-2022_40857840_2.xlsx"
-
-energy <- read_xlsx(fn, skip = 1) %>%
-  rename(
-    date = Datum,
-    low_el = `Meterstand laagtarief (El 1)`,
-    high_el = `Meterstand hoogtarief (El 2)`,
-    read = Meterstand
-  ) %>% select(date, low_el, high_el, read) %>% mutate(el = low_el + high_el)
-
-energy
-
-library(rstan)
-
-model <- stan_model("models/usage.stan")
-
-sampling(model, data = list(
-  N = 4, read = c(23, 24, 26, 38)
-))
-
 library(jsonlite)
-
-usage <- fromJSON("data/agg.json")
-
+library(ggplot2)
 library(readr)
 
+usage <- fromJSON("data/agg.json") %>% tibble %>%
+  mutate(
+    date = parse_datetime(date),
+    gas = gas_high + gas_low,
+    elec = elec_high + elec_low
+  ) %>%
+  select(-gas_high, -gas_low, -elec_high, -elec_low)
+
 usage %>%
-  mutate(date = parse_date(date)) %>%
-  ggplot(aes(x = date, y = gas_high)) +
+  ggplot(aes(x = date, y = gas)) +
   geom_point()
+
+usage %>%
+  ggplot(aes(x = date, y = elec)) +
+  geom_point()
+
+library(lubridate)
+
+usage %>%
+  group_by(date = floor_date(date, "day")) %>%
+  summarise(gas = sum(gas)) %>%
+  ggplot(aes(x = date, y = gas)) +
+  geom_line()
+
+usage %>%
+  group_by(hour = hour(date)) %>%
+  summarise(gas = sum(gas)) %>%
+  ggplot(aes(x = hour, y = gas)) +
+  geom_line()
+
+weather <- readr::read_csv(
+  "./data/uurgeg_260_2021-2030.zip",
+  skip = 30,
+  col_select = c(
+    date = YYYYMMDD,
+    hour = HH,
+    temp = "T",
+    radiation = Q
+  ),
+  col_types = cols(YYYYMMDD = col_date(format = "%Y%m%d"))
+  ) %>%
+  mutate(
+    date = parse_datetime(paste(date, sprintf("%02d", hour - 1))),
+    temp = temp * .1
+  ) %>%
+  select(-hour)
+
+df <- usage %>%
+  left_join(weather, by = c("date")) %>%
+  filter(date < parse_datetime("2023-02-27"))
+
+df %>% ggplot(aes(x = log(gas + 1), y = temp, color = radiation)) +
+  geom_point() + geom_smooth(color = "purple")
